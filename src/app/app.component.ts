fcc5ba351026f9134ed65afd45189e925bcc14ae
gordienko-andrei <gordi_enkoaa@yahoo.com>
2024-03-21 19:16:36 +0300

    headerSubheader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { SubheaderComponent } from './components/subheader/subheader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    HeaderComponent,
+    SubheaderComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
